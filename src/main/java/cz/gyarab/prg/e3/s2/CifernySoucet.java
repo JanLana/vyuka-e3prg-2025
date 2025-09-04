6ee496db4cb984930b064b5c5da28587e543255e
Jan Lana <jan.lana@gyarab.cz>
2025-09-04 11:35:22 +0200

    prvni hodina

---
+++ src/main/java/cz/gyarab/prg/e3/s2/CifernySoucet.java
@@ -0,0 +1,15 @@
+package cz.gyarab.prg.e3.s2;
+
+import java.util.Scanner;
+
+public class CifernySoucet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vstup = sc.nextByte();
+
+        int soucet = 0;
+
+        System.out.println("Ciferny soucet: " + soucet);
+
+    }
+}
